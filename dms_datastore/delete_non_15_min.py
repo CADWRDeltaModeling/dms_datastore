c2d43c17dc2a62dee1106b51b7939fe31459b2ec
Eli <Eli.Ateljevich@water.ca.gov>
2022-10-08 11:28:55 -0700

    Added file that deletes parts of stations that are not 15min. Very specialized, probably could be made less delta-specific.

---
+++ dms_datastore/delete_non_15_min.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-import pandas as pd
+import matplotlib.pyplot as plt
+from vtools.datastore.read_ts import *
+import os
+
+direct="raw/incoming"
+direct="raw"
+with open("non_15_min_files_checked.txt") as infile:
+    for line in infile:
+        if ":" in line:
+            filename = line.split(":")[0]
+            path = os.path.join(direct,filename)
+            if os.path.exists(path):
+                print(f"Removing {path}")
+                os.remove(path)
+            else:
+                print(f"Path not found: {path}")
