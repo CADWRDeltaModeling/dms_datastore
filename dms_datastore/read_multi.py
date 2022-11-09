85b7b62b40e5c92ab4f6cd25826d8738b7d003ab
Eli <Eli.Ateljevich@water.ca.gov>
2022-11-09 13:48:41 -0800

    Added read_ts_repo

--- dms_datastore/read_multi.py
+++ dms_datastore/read_multi.py
@@ -4,15 +4,49 @@
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
-from dms_datastore.read_ts import *
+from dms_datastore.read_ts import read_ts
+from dms_datastore import dstore_config
 import glob
 from vtools.functions.merge import ts_merge
+
+
+def read_ts_repo(station_id,variable,repo=None,src_priority=None):
+    """ Read time series data from a repository, prioritizing sources
+  
+    station_id : str
+        Station ID as defined in csv station file (see station_dbase.csv or station_info).
+        Possibly make it so that the agency or source id also works?
+
+    variable : str
+        Variable name as defined in variables.csv (not the agency name)
+        
+    repo : str
+        Name of repository. If it is a directory, will be used directly. Otherwise
+        it will be used to look up the name of the resitory using the dbase config file.
+        Finally if it is None, it will look up the default config file which is 
+        in the config file under the name 'repo'.
+        
+    """
+    if repo is None:
+        repository = dstore_config.config_file('repo')
+    elif os.path.exists(repo):
+        repository=repo
+    else:
+        repository = dstore_config_file(repo)
+
+    if src_priority is None:
+        src_priority = "*" #dstore_config.config("source_priority")
+    pats = []
+    print(pats)
+    for src in src_priority:
+        pats.append(os.path.join(repository,f"{src}_{station_id}_*_{variable}_*.*"))
+    return ts_multifile_read(pats) 
+    
 
 def ts_multifile_read(pats,transforms=None,selector=None,column_name=None):
 
     if not(isinstance(pats,list)):
         pats = [pats]
-    print("pats",pats)
     if transforms is None: transforms = [None]*len(pats)
     tss = []
     for fp,trans in zip(pats,transforms):
