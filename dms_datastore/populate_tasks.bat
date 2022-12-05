49a5c496f8c5366e9b5394ffb6b12026c898c3f6
Eli <Eli.Ateljevich@water.ca.gov>
2022-12-04 20:37:03 -0800

    current version of script that works in windows scheduler

--- dms_datastore/populate_tasks.bat
+++ dms_datastore/populate_tasks.bat
@@ -1,8 +1,30 @@
 
+echo started > log.txt
 
+echo activate >> log.txt
+call c:\Users\eli\miniconda3\Scripts\activate.bat py39
+
+echo deleting >> log.txt
+del /Q raw\* formatted\*
 echo populate
-populate_repo --dest=raw
-echo delete 
-delete_from_filelist dpath=raw 
+echo populate >> log.txt
+call populate_repo --dest=raw
+echo delete
+echo delete >> log.txt
+rem call delete_from_filelist --dpath=raw 
 echo reformat
-reformat --inpath raw --outpath formatted
+echo reformat >> log.txt
+call reformat --inpath raw --outpath formatted
+
+echo creating inventories
+echo inventory >> log.txt
+call inventory --repo formatted
+
+call compare_directories --base w:/continuous_station_repo_beta/raw --compare ./raw > compare_raw.txt
+
+call compare_directories --base w:/continuous_station_repo_beta/formatted_1yr --compare ./formatted > compare_formatted.txt
+
+
+
+echo done
+echo done >> log.txt
