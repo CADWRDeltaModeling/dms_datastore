95976cc266d4125b69b2a5acdca7cdab7bac618d
Nicky Sandhu <nicky.s.sandhu@gmail.com>
2024-01-05 10:46:42 -0800

    splitting auto screen into parallel stages to use agents

--- dms_datastore.auto_screen_pipeline.groovy
+++ dms_datastore.auto_screen_pipeline.groovy
@@ -9,27 +9,40 @@ pipeline {
         SRCDIR = 'formatted'
     }
     stages {
-        stage('mount network drive') {
+        stage('Mount Network Drive') {
             steps {
                 script {
                     // Define the network path and drive letter
                     def networkPath = '\\\\cnrastore-bdo\\modeling_data'
                     def driveLetter = 'Y:'
 
-                    // Check if the drive is already mounted
-                    def isMounted = bat(script: "if exist ${driveLetter} (echo true) else (echo false)", returnStdout: true).trim()
+                    // Execute the command and capture the output
+                    def script = '''
+                    @echo off
+                    if exist Y: (
+                        echo true
+                    ) else (
+                        echo false
+                    )
+                    '''
+                    def cmdOutput = bat(script: script, returnStdout: true).trim()
+                    echo "Command Output: '${cmdOutput}'"
 
-                    // Mount the network drive if it's not already mounted
-                    if (isMounted == 'false') {
+                    // Determine if the drive is mounted
+                    def isMounted = cmdOutput == 'true'
+
+                    if (!isMounted) {
+                        echo "Mounting ${driveLetter} to ${networkPath}"
                         bat "net use ${driveLetter} ${networkPath} /persistent:no"
                         echo "Mounted ${networkPath} as ${driveLetter}"
                     } else {
                         echo "${driveLetter} is already mounted."
                     }
+
+                    bat 'dir Y:\\'
                 }
             }
         }
-
         stage('ensure directories') {
             steps {
                 dir("${env.REPO_STAGING}/plots"){
@@ -40,48 +53,104 @@ pipeline {
                     }
                     writeFile file:'created.txt', text:CREATE_TIME
                 }
+                dir("${env.REPO_STAGING}/screened"){
+                    // write file with date created
+                    script {
+                        def now = new Date()
+                        CREATE_TIME=now.format("yyMMdd.HHmm", TimeZone.getTimeZone('UTC'))
+                    }
+                    writeFile file:'created.txt', text:CREATE_TIME
+                }
             }
         }
         stage('auto screen') {
-            steps {
-                dir("${env.REPO_STAGING}"){
-                    script {
-                        // Running commands in parallel
-                        parallel(
-                            elev: {
-                                bat "call conda activate dms_datastore & auto_screen --config %SCREEN_CONFIG% --fpath %SRCDIR% --dest screened --plot_dest plots --params elev"
-                            },
-                            flow: {
-                                bat "call conda activate dms_datastore & auto_screen --config %SCREEN_CONFIG% --fpath %SRCDIR% --dest screened --plot_dest plots --params flow"
-                            },
-                            ec: {
-                                bat "call conda activate dms_datastore & auto_screen --config %SCREEN_CONFIG% --fpath %SRCDIR% --dest screened --plot_dest plots --params ec"
-                            },
-                            temp: {
-                                bat "call conda activate dms_datastore & auto_screen --config %SCREEN_CONFIG% --fpath %SRCDIR% --dest screened --plot_dest plots --params temp"
-                            },
-                            turbidity: {
-                                bat "call conda activate dms_datastore & auto_screen --config %SCREEN_CONFIG% --fpath %SRCDIR% --dest screened --plot_dest plots --params turbidity"
-                            },
-                            ssc: {
-                                bat "call conda activate dms_datastore & auto_screen --config %SCREEN_CONFIG% --fpath %SRCDIR% --dest screened --plot_dest plots --params ssc"
-                            },
-                            ph: {
-                                bat "call conda activate dms_datastore & auto_screen --config %SCREEN_CONFIG% --fpath %SRCDIR% --dest screened --plot_dest plots --params ph"
-                            },
-                            do: {
-                                bat "call conda activate dms_datastore & auto_screen --config %SCREEN_CONFIG% --fpath %SRCDIR% --dest screened --plot_dest plots --params do"
-                            },
-                            predictions: {
-                                bat "call conda activate dms_datastore & auto_screen --config %SCREEN_CONFIG% --fpath %SRCDIR% --dest screened --plot_dest plots --params predictions"
-                            },
-                            cla: {
-                                bat "call conda activate dms_datastore & auto_screen --config %SCREEN_CONFIG% --fpath %SRCDIR% --dest screened --plot_dest plots --params cla"
-                            },
-                            velocity: {
-                                bat "call conda activate dms_datastore & auto_screen --config %SCREEN_CONFIG% --fpath %SRCDIR% --dest screened --plot_dest plots --params velocity"
-                            }
-                        )
+            parallel {
+                stage('Elev') {
+                    agent any
+                    steps {
+                        dir("${env.REPO_STAGING}"){
+                            bat "call conda activate dms_datastore & auto_screen --config %SCREEN_CONFIG% --fpath %SRCDIR% --dest screened --plot_dest plots --params elev"
+                        }
+                    }
+                }
+                stage('Flow') {
+                    agent any
+                    steps {
+                        dir("${env.REPO_STAGING}"){
+                            bat "call conda activate dms_datastore & auto_screen --config %SCREEN_CONFIG% --fpath %SRCDIR% --dest screened --plot_dest plots --params flow"
+                        }
+                    }
+                }
+                stage('Ec') {
+                    agent any
+                    steps {
+                        dir("${env.REPO_STAGING}"){
+                            bat "call conda activate dms_datastore & auto_screen --config %SCREEN_CONFIG% --fpath %SRCDIR% --dest screened --plot_dest plots --params ec"
+                        }
+                    }
+                }
+                stage('Temp') {
+                    agent any
+                    steps {
+                        dir("${env.REPO_STAGING}"){
+                            bat "call conda activate dms_datastore & auto_screen --config %SCREEN_CONFIG% --fpath %SRCDIR% --dest screened --plot_dest plots --params temp"
+                        }
+                    }
+                }
+                stage('Turbidity') {
+                    agent any
+                    steps {
+                        dir("${env.REPO_STAGING}"){
+                            bat "call conda activate dms_datastore & auto_screen --config %SCREEN_CONFIG% --fpath %SRCDIR% --dest screened --plot_dest plots --params turbidity"
+                        }
+                    }
+                }
+                stage('Ssc') {
+                    agent any
+                    steps {
+                        dir("${env.REPO_STAGING}"){
+                            bat "call conda activate dms_datastore & auto_screen --config %SCREEN_CONFIG% --fpath %SRCDIR% --dest screened --plot_dest plots --params ssc"
+                        }
+                    }
+                }
+                stage('Ph') {
+                    agent any
+                    steps {
+                        dir("${env.REPO_STAGING}"){
+                            bat "call conda activate dms_datastore & auto_screen --config %SCREEN_CONFIG% --fpath %SRCDIR% --dest screened --plot_dest plots --params ph"
+                        }
+                    }
+                }
+                stage('Do') {
+                    agent any
+                    steps {
+                        dir("${env.REPO_STAGING}"){
+                            bat "call conda activate dms_datastore & auto_screen --config %SCREEN_CONFIG% --fpath %SRCDIR% --dest screened --plot_dest plots --params do"
+                        }
+                    }
+                }
+                stage('Predictions') {
+                    agent any
+                    steps {
+                        dir("${env.REPO_STAGING}"){
+                            bat "call conda activate dms_datastore & auto_screen --config %SCREEN_CONFIG% --fpath %SRCDIR% --dest screened --plot_dest plots --params predictions"
+                        }
+                    }
+                }
+                stage('Cla') {
+                    agent any
+                    steps {
+                        dir("${env.REPO_STAGING}"){
+                            bat "call conda activate dms_datastore & auto_screen --config %SCREEN_CONFIG% --fpath %SRCDIR% --dest screened --plot_dest plots --params cla"
+                        }
+                    }
+                }
+                stage('Velocity') {
+                    agent any
+                    steps {
+                        dir("${env.REPO_STAGING}"){
+                            bat "call conda activate dms_datastore & auto_screen --config %SCREEN_CONFIG% --fpath %SRCDIR% --dest screened --plot_dest plots --params velocity"
+                        }
                     }
                 }
             }
