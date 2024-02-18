5c7dfdcad0f0b20076ae3ca615dd7be5c379dbb8
Eli <Eli.Ateljevich@water.ca.gov>
2024-02-18 08:35:01 -0800

    Revise example script for populating using desktop. Jenkins procedures are different, more fragmented and target a slightly smaller computer (4 core).

--- dms_datastore/populate_tasks.bat
+++ dms_datastore/populate_tasks.bat
@@ -1,10 +1,13 @@
+rem This file represents data acquisition and reformatting used in the repo
+rem when done on a standalone computer
 
 echo started > log.txt
 
 set repo=\\cnrastore-bdo\Modeling_Data\repo\continuous
 set repo_staging=\\cnrastore-bdo\Modeling_Data\repo_staging\continuous
+
 echo activate >> log.txt
-call c:\Users\eli\miniconda3\Scripts\activate.bat schism4
+call c:\Users\eli\miniconda3\Scripts\activate.bat schism
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 
@@ -24,15 +27,16 @@ call reformat --inpath raw --outpath formatted >> populate_log.txt
 echo process usgs
 echo splitting or consolidating usgs multi column files >> log.txt
 usgs_multi --fpath formatted  >> populate_log.txt
+
 echo creating inventories
 echo inventory >> log.txt
 call inventory --repo formatted
 
 rem At this point format gets checked
-call compare_directories --base %repo%/raw --compare %repo_staging%/raw > compare_raw.txt
 
-call compare_directories --base %repo%/formatted --compare %repo_staging%/formatted > compare_formatted.txt
-call compare_directories --base %repo%/screened --compare %repo_staging%/screened > compare_screened.txt
+call compare_directories --base %repo%/raw --compare %repo_staging%/raw --excepts exceptions.csv --outfile compare_raw.txt
+call compare_directories --base %repo%/formatted --compare %repo_staging%/formatted --excepts exceptions.csv --outfile compare_formatted.txt
+call compare_directories --base %repo%/screened --compare %repo_staging%/screened --excepts exceptions.csv --outfile compare_screened.txt
 
 
 echo done
