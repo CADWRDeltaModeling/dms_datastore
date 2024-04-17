c676390e62052137cd38eef2f82b4a01cfb01d91
Shu <Qiang.Shu@water.ca.gov>
2024-04-17 09:29:30 -0700

    dowload up to today by default, pep8 formatting

--- dms_datastore/download_hrrr.py
+++ dms_datastore/download_hrrr.py
@@ -7,6 +7,7 @@ import argparse
 from dms_datastore.hrrr3 import *
 import datetime
 
+
 def create_arg_parser():
     """ Create an argument parser
         return: argparse.ArgumentParser
@@ -15,54 +16,56 @@ def create_arg_parser():
     # Read in the input file
     parser = argparse.ArgumentParser(
         description="""
-                       Download Download NOAA High-Resolution Rapid Refresh (HRRR) Model using AWS bucket service.
-                       
-                     Usage:
-                           download_hrr.py 01/01/2023  g:\temp 15  37.3  39.0 -123.15 -121.1
+        Download Download NOAA High-Resolution Rapid Refresh
+        (HRRR) Model using AWS bucket service.
+        Usage:
+        download_hrrr 01/01/2023  g:\temp 15  37.36  39.0 -123.023 -121.16
                      """)
-    parser.add_argument('start_date', default=None,
-                        help='starting date of HRRR data, must be format like 09/19/2018')
-    parser.add_argument('destination', default=None,
+    parser.add_argument('--start_date', default=None, required=True,
+                        help='starting date of HRRR data, must be \
+                        format like 09/19/2018')
+    parser.add_argument('--destination', default=None, required=True,
                         help='path to store downloaded HRRR data')
-    parser.add_argument('rnday', default=None,type=int,
-                        help="number of days of data to be downloaded")
-    parser.add_argument('latitude_min', default=None,type=float,
-                        help='Minimal latitude of bounding box for raw data to be downloaded')
-    parser.add_argument('latitude_max', default=None,type=float,
-                        help='Maximal latitude of bounding box for raw data to be downloaded')
-    parser.add_argument('longitude_min', default=None,type=float,
-                        help='Minimal longititude of bounding box for raw data to be downloaded')
-    parser.add_argument('longitude_max', default=None,type=float,
-                        help='Maximal longititude of bounding box for raw data to be downloaded')
-    
-    
+    parser.add_argument('--rnday', default=None, type=int, required=False,
+                        help="number of days of data to be downloaded,\
+                            if not given download up to today")
+    parser.add_argument('--latitude_min', default=37.36, type=float,
+                        required=False, help='Minimal latitude of bounding box\
+                            for raw data to be downloaded,default 37.36')
+    parser.add_argument('--latitude_max', default=39.0, type=float,
+                        required=False, help='Maximal latitude of bounding box\
+                            for raw data to be downloaded,default 39.0')
+    parser.add_argument('--longitude_min', default=-123.023, type=float,
+                        required=False, help='Minimal longititude of bounding\
+                        box for raw data to be downloaded,default -123.023')
+    parser.add_argument('--longitude_max', default=-121.16, type=float,
+                        required=False, help='Maximal longititude of bounding\
+                            box for raw data to be downloaded,default -121.16')
+
     return parser
 
-def download_hrrr(start_date,rnday,pscr,bbox):
-    
-    hr3=HRRR(start_date=start_date,rnday=rnday,pscr=pscr,bbox=bbox)
+
+def download_hrrr(start_date, rnday, pscr, bbox):
+
+    hr3 = HRRR(start_date=start_date, rnday=rnday, pscr=pscr, bbox=bbox)
 
 
-    
 def main():
     """ Main function
     """
     parser = create_arg_parser()
     args = parser.parse_args()
 
-    bbox=[args.longitude_min,args.latitude_min,args.longitude_max,args.latitude_max]
-    pscr=args.destination
-    rnday=args.rnday
-    start_date= datetime.datetime.strptime(args.start_date, '%m/%d/%Y')
-    download_hrrr(start_date,rnday,pscr,bbox)
+    bbox = [args.longitude_min, args.latitude_min, args.longitude_max,
+            args.latitude_max]
+    pscr = args.destination
+    rnday = args.rnday
+
+    start_date = datetime.datetime.strptime(args.start_date, '%m/%d/%Y')
+    if rnday is None:
+        rnday = (datetime.datetime.now() - start_date).days + 1
+    download_hrrr(start_date, rnday, pscr, bbox)
+
 
 if __name__ == "__main__":
     main()
-
-    
-#bbox=[-123.15,37.3,-121.1,39.0]
-#pscr="G:\\temp"
-#rnday=5
-#import datetime as dtm
-#t0=dtm.datetime(2022,3,1)
-#hr3=HRRR(start_date=t0,rnday=rnday,pscr=pscr,bbox=bbox)
