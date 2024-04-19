8c265bcf966fb47ebd4a34b892ffe51156ee5036
Shu <Qiang.Shu@water.ca.gov>
2024-04-19 10:11:19 -0700

    change hrrr download api conforming to existing downloading script style

--- dms_datastore/download_hrrr.py
+++ dms_datastore/download_hrrr.py
@@ -19,26 +19,26 @@ def create_arg_parser():
         Download Download NOAA High-Resolution Rapid Refresh
         (HRRR) Model using AWS bucket service.
         Usage:
-        download_hrrr 01/01/2023  g:\temp 15  37.36  39.0 -123.023 -121.16
+        download_hrrr --sdate 2018-02-19  --dest /path/to/modeling_data
                      """)
-    parser.add_argument('--start_date', default=None, required=True,
+    parser.add_argument('--sdate', default=None, required=True,
                         help='starting date of HRRR data, must be \
-                        format like 09/19/2018')
-    parser.add_argument('--destination', default=None, required=True,
+                        format like 2018-02-19')
+    parser.add_argument('--dest', default=None, required=True,
                         help='path to store downloaded HRRR data')
-    parser.add_argument('--rnday', default=None, type=int, required=False,
-                        help="number of days of data to be downloaded,\
+    parser.add_argument('--edate', default=None, required=False,
+                        help="end date for the record to be downloaded,\
                             if not given download up to today")
-    parser.add_argument('--latitude_min', default=37.36, type=float,
+    parser.add_argument('--lat_min', default=37.36, type=float,
                         required=False, help='Minimal latitude of bounding box\
                             for raw data to be downloaded,default 37.36')
-    parser.add_argument('--latitude_max', default=39.0, type=float,
+    parser.add_argument('--lat_max', default=39.0, type=float,
                         required=False, help='Maximal latitude of bounding box\
                             for raw data to be downloaded,default 39.0')
-    parser.add_argument('--longitude_min', default=-123.023, type=float,
+    parser.add_argument('--lon_min', default=-123.023, type=float,
                         required=False, help='Minimal longititude of bounding\
                         box for raw data to be downloaded,default -123.023')
-    parser.add_argument('--longitude_max', default=-121.16, type=float,
+    parser.add_argument('--lon_max', default=-121.16, type=float,
                         required=False, help='Maximal longititude of bounding\
                             box for raw data to be downloaded,default -121.16')
 
@@ -56,14 +56,16 @@ def main():
     parser = create_arg_parser()
     args = parser.parse_args()
 
-    bbox = [args.longitude_min, args.latitude_min, args.longitude_max,
-            args.latitude_max]
-    pscr = args.destination
-    rnday = args.rnday
+    bbox = [args.lon_min, args.lat_min, args.lon_max, args.lat_max]
+    pscr = args.dest
+    end_date = args.edate
 
-    start_date = datetime.datetime.strptime(args.start_date, '%m/%d/%Y')
-    if rnday is None:
-        rnday = (datetime.datetime.now() - start_date).days + 1
+    start_date = datetime.datetime.strptime(args.sdate, '%Y-%m-%d')
+    if end_date is None:
+        end_date = datetime.datetime.now()
+    else:
+        end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d')
+    rnday = (datetime.datetime.now() - start_date).days + 1
     download_hrrr(start_date, rnday, pscr, bbox)
 
 
