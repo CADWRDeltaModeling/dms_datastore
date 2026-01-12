51ab861dc97670609cd229de21d19feddd5c0632
Eli <Eli.Ateljevich@water.ca.gov>
2026-01-12 15:58:24 -0800

    Trivial commit to test conda uploading.

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 
 # setup.py only needed for conda to resolve versioning
 # DO NOT ADD ANYTHING ELSE HERE
-
 setup(
     name="dms_datastore",
     version=get_version(),
